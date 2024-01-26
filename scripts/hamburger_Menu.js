cdfd8634972799d8870cc7eb5ac98cc55e588887
ValRaz <valerie.dres@gmail.com>
2024-01-25 22:31:43 -0700

    responsive navigation push

--- scripts/hamburger_Menu.js
+++ scripts/hamburger_Menu.js
@@ -1,17 +1,23 @@
-const hamButton = document.querySelector('#menu');
-const navigation = document.querySelector('.navigation');
+document.addEventListener('DOMContentLoaded', function() {
+    const hamButton = document.querySelector('#menu');
+    const navigation = document.querySelector('.navigation');
 
-hamButton.addEventListener('click', () => {
-	navigation.classList.toggle('open');
-	hamButton.classList.toggle('open');
-});
-
-const navLinks = document.querySelectorAll('.navigation a');
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        navigation.classList.remove('open');
-        hamButton.classList.remove('open');
+    hamButton.addEventListener('click', () => {
+        navigation.classList.toggle('open');
+        hamButton.classList.toggle('open');
     });
 
-    link.classList.add('active');
+    const navLinks = document.querySelectorAll('.navigation a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            navigation.classList.remove('open');
+            hamButton.classList.remove('open');
+
+            navLinks.forEach(navLink => {
+                navLink.classList.remove('active');
+            });
+
+            link.classList.add('active');
+        });
+    });
 });
