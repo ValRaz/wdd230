f5e9cf73a2f18a9944a37560f14c8d3563aac3e0
ValRaz <valerie.dres@gmail.com>
2024-01-25 20:17:16 -0700

    hamburger menu first push

---
+++ scripts/hamburger_Menu.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const nav = document.querySelector('nav');
+
+    const hamburgerBtn = document.createElement('button');
+    hamburgerBtn.classList.add('hamburger');
+    hamburgerBtn.setAttribute('aria-label', 'Menu');
+    hamburgerBtn.innerHTML = '<span class="hamburger-icon">&#9776;</span><span class="close-icon">&times;</span>';
+
+    const closeIcon = hamburgerBtn.querySelector('.close-icon');
+    closeIcon.style.display = 'none';
+
+    nav.appendChild(hamburgerBtn);
+
+    hamburgerBtn.addEventListener('click', function() {
+        const menu = document.querySelector('nav ul');
+        menu.classList.toggle('show-menu');
+    });
+});
