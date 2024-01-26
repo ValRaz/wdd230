53d7a221a63be727a070442e0d6d9b870395acea
ValRaz <valerie.dres@gmail.com>
2024-01-25 21:01:35 -0700

    hamburger button styles and troubleshooting

--- scripts/hamburger_Menu.js
+++ scripts/hamburger_Menu.js
@@ -11,8 +11,22 @@ document.addEventListener('DOMContentLoaded', function(){
 
     nav.appendChild(hamburgerBtn);
 
+    const dropdownMenu = document.createElement('div');
+    dropdownMenu.classList.add('dropdown-menu');
+    
+    const clonedNav = nav.querySelector('ul').cloneNode(true);
+    dropdownMenu.appendChild(clonedNav);
+
+    document.body.appendChild(dropdownMenu);
+
     hamburgerBtn.addEventListener('click', function() {
-        const menu = nav.querySelector('ul');
+        const menu = document.querySelector('nav ul');
         menu.classList.toggle('show-menu');
+        dropdownMenu.classList.toggle('show-menu');
+        if (dropdownMenu.classList.contains('show-menu')) {
+            closeIcon.style.display = 'block';
+        } else {
+            closeIcon.style.display = 'none';
+        }
     });
 });
