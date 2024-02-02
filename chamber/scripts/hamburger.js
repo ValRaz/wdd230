714c88b6c14b568f9b690bae47ef4269f2329649
ValRaz <valerie.dres@gmail.com>
2024-02-01 21:17:05 -0700

    Hamburger menu js push

---
+++ chamber/scripts/hamburger.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const hamButton = document.querySelector('#menu');
+    const navigation = document.querySelector('.navigation');
+
+    hamButton.addEventListener('click', () => {
+        navigation.classList.toggle('open');
+        hamButton.classList.toggle('open');
+    });
+
+    const navLinks = document.querySelectorAll('.navigation a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            navigation.classList.remove('open');
+            hamButton.classList.remove('open');
+
+            navLinks.forEach(navLink => {
+                navLink.classList.remove('active');
+            });
+
+            link.classList.add('active');
+        });
+    });
+});
