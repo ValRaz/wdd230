3ff9b4399468ccf9d669e1123e5f04bd8634953e
ValRaz <valerie.dres@gmail.com>
2024-02-03 14:34:30 -0700

    hamburger menu troubleshooting

--- chamber/scripts/hamburger.js
+++ chamber/scripts/hamburger.js
@@ -1,17 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
     const hamButton = document.querySelector('#menu');
-    const navigation = document.querySelector('.navigation ul');
+    const navigation = document.querySelector('.navigation');
 
     hamButton.addEventListener('click', () => {
         navigation.classList.toggle('open');
-        hamButton.classList.toggle('open');
     });
 
-    const navLinks = document.querySelectorAll('.navigation a');
+    const navLinks = document.querySelectorAll('.navigation ul li');
     navLinks.forEach(link => {
         link.addEventListener('click', () => {
             navigation.classList.remove('open');
-            hamButton.classList.remove('open');
 
             navLinks.forEach(navLink => {
                 navLink.classList.remove('active');
