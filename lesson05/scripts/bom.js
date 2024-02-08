d58ed7b465509a61d91bfd0921af754c7c8d72fd
ValRaz <valerie.dres@gmail.com>
2024-02-07 17:21:35 -0700

    lesson 6 push day two

--- lesson05/scripts/bom.js
+++ lesson05/scripts/bom.js
@@ -3,8 +3,24 @@ const addButton = document.querySelector('button');
 const listElement = document.getElementById('list');
 
 addButton.Button.addEventListener('click', () => {
-    if(inputElement.ariaValueMax.trim() === '') {
+    if(inputElement.value.trim() === '') {
         inputElement.focus();
         return;
     }
+});
+
+const li = document.createElement('li');
+listElement.appendChild(li);
+li.textContent = inputElement.value.trim();
+
+const deleteButton = document.createElement('button');
+deleteButton.textContent = '❌';
+deleteButton.classList.add('delete');
+
+li.append(deleteButton);
+
+document.getElementById('list').appendChild(li);
+
+deleteButton.addEventListener('click', () => {
+    ul.removeChilde(li);
 });
