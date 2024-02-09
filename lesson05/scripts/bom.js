b69647b2e07cf52012d55c5c5a4233ce749008ed
ValRaz <valerie.dres@gmail.com>
2024-02-08 21:25:29 -0700

    js final push

--- lesson05/scripts/bom.js
+++ lesson05/scripts/bom.js
@@ -1,30 +1,31 @@
-const inputt = document.querySelector('#favchap');
+const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('list');
+const list = document.querySelector('ul'); 
 
 button.addEventListener('click', () => {
-    if(input.value != '') {
+   
+    if (input.value.trim() !== '') {
+        const li = document.createElement('li');
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = '❌';
+        deleteButton.classList.add('delete'); 
+
+        li.textContent = input.value;
+
+        li.appendChild(deleteButton);
+
+        list.appendChild(li);
+
+        deleteButton.addEventListener('click', () => {
+            list.removeChild(li);
+            input.focus();
+        });
+
+        input.value = '';
+
         input.focus();
-        return;
+    } else {
+        input.focus();
     }
-
-    const li = document.createElement('li');
-
-    li.textContent = input.value;
-
-    const deleteButton = document.createElement('button');
-    deleteButton.textContent = '❌';
-    deleteButton.classList.add('delete');
-
-    li.appendChild(deleteButton);
-
-    list.append(li);
-
-    input.value = '';
-
-    input.focus();
-
-    deleteButton.addEventListener('click', () => {
-        list.removeChild(li);
-    });
 });
