a9c79c5c7ace7bcc0cff11032cc0cbb0ea1376f5
ValRaz <valerie.dres@gmail.com>
2024-02-10 10:56:29 -0700

    darkmode toggle and contact info added

---
+++ chamber/scripts/dmtoggle.js
@@ -0,0 +1,7 @@
+document.getElementById('darkModeToggle').addEventListener('change', function() {
+    if (this.checked) {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+});
