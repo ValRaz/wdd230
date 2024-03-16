b59caf70471fe84ea5c5ec58a8e7a77b1c8ceda6
ValRaz <valerie.dres@gmail.com>
2024-03-16 13:25:54 -0600

    banner js complete

--- chamber/scripts/banner.js
+++ chamber/scripts/banner.js
@@ -1,6 +1,14 @@
 let dismissBanner = document.getElementById('dismiss-banner');
 let banner = document.getElementById('banner');
 
+let currentDayOfWeek = new Date().getDay();
+
+if (currentDayOfWeek >= 1 && currentDayOfWeek <= 3) {
+    banner.style.display = 'flex';
+} else {
+    banner.style.display = 'none';
+}
+
 dismissBanner.addEventListener('click', () => {
     banner.style.display = 'none';
 });
