72873a1e6841df3f4cf618e5e71d1f5dfdaa9535
ValRaz <valerie.dres@gmail.com>
2024-04-01 18:05:48 -0600

    js update

---
+++ scootsFinalProject/scripts/rental-info.js
@@ -0,0 +1,37 @@
+const baseUrl ='https://valraz.github.io/wdd230/scootsFinalProject/'
+const url = 'https://valraz.github.io/wdd230/scootsFinalProject/data/prices.json'
+
+function populateTable(rentals) {
+    const tableBody = document.querySelector('tbody');
+    rentals.forEach(rental => {
+        const row = document.createElement('tr');
+        row.innerHTML = `<td>${rental.type}</td>
+            <td>${rental.max_persons}</td>
+            <td>${rental.half_day_reservation}</td>
+            <td>${rental.full_day_reservation}</td>
+            <td>${rental.half_day_walk_in}</td>
+            <td>${rental.full_day_walk_in}</td>`;
+        tableBody.appendChild(row);
+    });
+}
+
+function populateCards(rentals) {
+    const rentalCardsContainer = document.querySelector('.rental-cards');
+    rentals.forEach(rental => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.innerHTML = `
+            <img src="${rental.image}" alt="${rental.type}">
+            <h3>${rental.type}</h3>
+        `;
+        rentalCardsContainer.appendChild(card);
+    });
+}
+
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        populateTable(data.rentals);
+        populateCards(data.rentals);
+    })
+    .catch(error => console.error('Error fetching JSON:', error));
