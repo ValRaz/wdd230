5b85188aed11e72ca004b924c6333b645834aa2c
ValRaz <valerie.dres@gmail.com>
2024-04-01 18:24:06 -0600

    js cleanup

--- scootsFinalProject/scripts/rental-info.js
+++ scootsFinalProject/scripts/rental-info.js
@@ -6,7 +6,6 @@ function populateTable(rentals) {
     rentals.forEach(rental => {
         const row = document.createElement('tr');
         row.innerHTML = `<td>${rental.type}</td>
-            <td>${rental.max_persons}</td>
             <td>${rental.half_day_reservation}</td>
             <td>${rental.full_day_reservation}</td>
             <td>${rental.half_day_walk_in}</td>
